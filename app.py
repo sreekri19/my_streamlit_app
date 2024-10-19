f380f157e43e119d3800e4181fc6fce083279ccd
krishnasrinivas chilkamarri <krishnasrinivaschilkamarri@krishnaivassAir.attlocal.net>
2024-10-19 09:20:25 -0500

    Fixed requirements.txt and import issues

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import fitz
+import fitz  # PyMuPDF's correct import name is 'fitz'
 from transformers import BertTokenizer, BertModel
 import torch
 from rapidfuzz import fuzz
